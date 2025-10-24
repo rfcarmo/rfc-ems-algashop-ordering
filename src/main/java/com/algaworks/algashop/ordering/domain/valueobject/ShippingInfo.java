436cdca292b3b609411d9474ce6f507b4b26782e
rfcarmo <rfortunatoc@gmail.com>
2025-10-24 12:45:41 +0100

    Mod2 - 6.12. Desafio: Implemente o Value Object de Money e Quantity.

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/ShippingInfo.java
@@ -0,0 +1,27 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+import java.util.Objects;
+
+/**
+ * ShippingInfo
+ * Regras de negócio
+ *  - Todos os campos são obrigatórios:
+ *      - FullName fullName
+ *      - Document document
+ *      - Phone phone
+ *      - Address address
+ * Implementação
+ *  - Utilize record com anotação @Builder.
+ *  - No construtor compacto, valide todos os campos com Objects.requireNonNull().
+ */
+
+public record ShippingInfo(FullName fullName, Document document, Phone phone, Address address) {
+
+    public ShippingInfo {
+        Objects.requireNonNull(fullName);
+        Objects.requireNonNull(document);
+        Objects.requireNonNull(phone);
+        Objects.requireNonNull(address);
+    }
+
+}
