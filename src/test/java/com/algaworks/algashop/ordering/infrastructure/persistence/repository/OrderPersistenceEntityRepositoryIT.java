dccbf62c524c28ab7f0608d9315e67de68d1a9a9
rfcarmo <rfortunatoc@gmail.com>
2025-10-31 08:17:59 +0000

    Mod2 - 8.9. Testando modelo de persistência com testes de integração.

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,12 +1,51 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
+import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 @SpringBootTest
+@Transactional
 class OrderPersistenceEntityRepositoryIT {
 
+    private final OrderPersistenceEntityRepository repository;
+
+    @Autowired
+    OrderPersistenceEntityRepositoryIT(OrderPersistenceEntityRepository repository) {
+        this.repository = repository;
+    }
+
     @Test
-    public void shouldRun() {}
+    public void shouldRun() {
+        long orderId = IdGenerator.generateTSID().toLong();
+
+        OrderPersistenceEntity entity = OrderPersistenceEntity.builder()
+                .id(orderId)
+                .customerId(IdGenerator.generateTimeBasedUUID())
+                .totalItems(2)
+                .totalAmount(new BigDecimal(1000))
+                .status("PLACED")
+                .paymentMethod("CREDIT_CARD")
+                .placedAt(OffsetDateTime.now())
+                .build();
+
+        repository.saveAndFlush(entity);
+
+        Assertions.assertThat(repository.existsById(orderId)).isTrue();
+    }
+
+    @Test
+    public void shouldCount() {
+        long ordersCount = repository.count();
+
+        Assertions.assertThat(ordersCount).isZero();
+    }
 
 }
