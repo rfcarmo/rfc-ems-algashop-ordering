0f5cb2eb95ec1b644dd77579b4e12ee35bda7238
rfcarmo <rfortunatoc@gmail.com>
2025-11-06 18:10:05 +0000

    Mod2 - 10.5. Integração com Spring RestClient.

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/ShoppingCostServiceFakeImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/ShoppingCostServiceFakeImpl.java
@@ -2,9 +2,13 @@ package com.algaworks.algashop.ordering.infrastructure.fake;
 
 import com.algaworks.algashop.ordering.domain.model.service.ShippingCostService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+@Component
+@ConditionalOnProperty(name = "algashop.integrations.shipping.provider", havingValue = "FAKE")
 public class ShoppingCostServiceFakeImpl implements ShippingCostService {
 
     @Override
